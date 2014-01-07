933c1fbc782ac5298488b36e08d8e986db68254d
App Academy Student <student@appacademy.io>
2014-01-07 11:10:55 -0500

    Adds board rendering, puts pieces in the rows array of Board.

--- board.rb
+++ board.rb
@@ -1,4 +1,7 @@
+require "./pieces"
+
 class Board
+  attr_accessor :rows, :pieces
 
   def initialize
     @rows = Board.blank_grid
@@ -9,21 +12,57 @@ class Board
     Array.new(8) { Array.new(8) { nil } }
   end
 
+
   def populate_board
-    white_rook_positions = [[0,0], [0,7], [7,0], [7,7]]
+    rook_positions = [[0,0], [0,7], [7,0], [7,7]]
     knight_positions = [[1,0], [6,0], [1,7], [6,7]]
     bishop_positions = [[2,0], [5,0], [2,7], [5,7]]
-    queen_positions = [[3,0], [3,7]]
-    king_positions = [[4,0], [4,7]]
 
+    # white pieces
     (0..7).each do |x|
-      (0..7).each do |y|
-        if rook_positions.include?([x,y])
-          self[x,y]
+      if rook_positions.include?([x,0])
+        self[x,0] = Rook.new(self, [x, 0], :w)
+      elsif knight_positions.include?([x,0])
+        self[x,0] = Knight.new(self, [x, 0], :w)
+      elsif bishop_positions.include?([x, 0])
+        self[x,0] = Bishop.new(self, [x, 0], :w)
       end
     end
+    self[3,0] = Queen.new(self, [3, 0], :w)
+    self[4,0] = King.new(self, [4, 0], :w)
+
+    #making all pawns
+    (0..7).each do |x|
+      self[x,1] = Pawn.new(self, [x, 1], :w)
+      self[x,6] = Pawn.new(self, [x, 6], :b)
+    end
+
+    (0..7).each do |x|
+      if rook_positions.include?([x,7])
+        self[x,7] = Rook.new(self, [x, 7], :b)
+      elsif knight_positions.include?([x,7])
+        self[x,7] = Knight.new(self, [x, 7], :b)
+      elsif bishop_positions.include?([x, 7])
+        self[x,7] = Bishop.new(self, [x, 7], :b)
+      end
+    end
+    self[3,7] = Queen.new(self, [3, 7], :b)
+    self[4,7] = King.new(self, [4, 7], :b)
+
   end
 
+  def render
+    display_board = []
+
+    self.rows.each do |row|
+      display_row = row.map{|piece| piece.nil? ? nil : piece.symbol}
+      display_board << display_row
+    end
+
+    display_board.each do |row|
+      puts row.inspect
+    end
+  end
 
   def [](x, y)
     self.rows[y][x]
--- pieces.rb
+++ pieces.rb
@@ -1,5 +1,5 @@
 class Piece
-  attr_accessor :board, :pos, :color
+  attr_accessor :board, :pos, :color, :symbol
 
   def initialize(board, pos, color)
     @board, @pos, @color = board, pos, color
@@ -12,15 +12,28 @@ end
 class SlidingPiece < Piece
   attr_accessor :move_dirs
 
-  def initialize
-    super(board, pos)
+  def initialize(board, pos, color)
+    super(board, pos, color)
     @move_dirs = self.class.move_dirs
   end
 
+  #need to finish this
   def moves
     possible_moves = []
 
     move_dirs.each do |dir|
+      num_steps = 1
+
+      loop do
+        next_step = [dir[0]*num_steps, dir[1]*num_steps]
+        next_pos = [pos[0] + next_step, pos[1] + next_step]
+
+        unless (next_pos[0].between?(0..7) && next_pos[1].between?(0..7))
+          break
+        end
+
+
+      end
 
     end
   end
@@ -30,6 +43,10 @@ class SteppingPiece < Piece
 end
 
 class Bishop < SlidingPiece
+  def initialize(board, pos, color)
+    super(board, pos, color)
+    self.symbol = :b
+  end
 
   def self.move_dirs
     [[1, 1], [1, -1], [-1, 1], [-1, -1]]
@@ -37,6 +54,10 @@ class Bishop < SlidingPiece
 end
 
 class Rook < SlidingPiece
+  def initialize(board, pos, color)
+    super(board, pos, color)
+    self.symbol = :r
+  end
 
   def self.move_dirs
     [[1, 0], [-1, 0], [0, 1], [0, -1]]
@@ -44,6 +65,10 @@ class Rook < SlidingPiece
 end
 
 class Queen < SlidingPiece
+  def initialize(board, pos, color)
+    super(board, pos, color)
+    self.symbol = :q
+  end
 
   def self.move_dirs
     Rook.move_dirs + Bishop.move_dirs
@@ -51,7 +76,22 @@ class Queen < SlidingPiece
 end
 
 class Knight < SteppingPiece
+  def initialize(board, pos, color)
+    super(board, pos, color)
+    self.symbol = :n
+  end
 end
 
 class King < SteppingPiece
+  def initialize(board, pos, color)
+    super(board, pos, color)
+    self.symbol = :k
+  end
+end
+
+class Pawn < Piece
+  def initialize(board, pos, color)
+    super(board, pos, color)
+    self.symbol = :p
+  end
 end
